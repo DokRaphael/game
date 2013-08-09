c06a26f74d4163f9bae24d16cbca37efd4d12dc6
dook0 <dook0@hotmail.com>
2013-08-09 10:46:27 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,304 +55,299 @@ var initPhoneController = function()
 
 			//socket.on("welcome", function()
 			//{
-				socket.emit("device",{"type":"controller"});
+			socket.emit("device",{"type":"controller"});
 			//});
 			// When game code is validated, we can begin playing...
 			console.log("controller");
 	
 			//socket.on("connected", function(data)
 			//{
-				console.log("connected");
+			console.log("connected");
 
-				window.addEventListener('deviceorientation', function(event) 
-				{
-				   var a = Math.round(event.alpha); // "direction"
-				   var b = Math.round(event.beta); // left/right 'tilt'
-				   var g = Math.round(event.gamma); // forward/back 'tilt'
+			window.addEventListener('deviceorientation', function(event) 
+			{
+			   var a = Math.round(event.alpha); // "direction"
+			   var b = Math.round(event.beta); // left/right 'tilt'
+			   var g = Math.round(event.gamma); // forward/back 'tilt'
 
-				   // Regardless of phone direction, 
-				   //  left/right tilt should behave the same
-				   var turn = b;
-					console.log("beta :"+b); 
-				   // Tell game to turn the vehicle
-				   socket.emit("turn", {'turn':turn, 'g':b});
-				}, false);
-				
-				
-				window.ondevicemotion = function(event) 
-				{
-					ax = event.accelerationIncludingGravity.x
-					ay = event.accelerationIncludingGravity.y
-					az = event.accelerationIncludingGravity.z
-					rotation = event.rotationRate;
-					if (rotation != null) 
-					{
-						arAlpha = Math.round(rotation.alpha);
-						arBeta = Math.round(rotation.beta);
-						arGamma = Math.round(rotation.gamma);
-					}
-					if(Math.abs(arBeta)>160)
-					{
-						//moveCharJump(event);
-					}
-				}
-				window.ondeviceorientation = function(event) 
-				{
-					alpha = Math.round(event.alpha);
-					beta = Math.round(event.beta);
-					gamma = Math.round(event.gamma);
+			   // Regardless of phone direction, 
+			   //  left/right tilt should behave the same
+			   var turn = b;
+				console.log("beta :"+b); 
+			   // Tell game to turn the vehicle
+			   socket.emit("turn", {'turn':turn, 'g':b});
+			}, false);
 			
-					if(beta > 15)
-					{
-						//moveCharRight(event);
-						socket.emit("right",beta);
-					}
-					else if(beta<-15)
-					{
-						socket.emit("left",beta);
-						//moveCharLeft(event);
-					}
-					else
-					{
-						socket.emit("stop",beta);
-						//stopCharMob(event);
-					}
-		
-				}
-					// Hide game code input, and show the vehicle wheel UI						
-					// If user touches the screen, accelerate
-					document.addEventListener("touchstart", function(event)
-					{
-					   socket.emit("accelerate", {'accelerate':true});
-					   $('#forward').addClass('active');
-
-					}, false);
-
-					// Stop accelerating if user stops touching screen
-					document.addEventListener("touchend", function(event)
-					{
-					   socket.emit("accelerate", {'accelerate':false});
-					   $('#forward').removeClass('active');
-					}, false);
-
-					// Prevent touchmove event from cancelling the 'touchend' event above
-					document.addEventListener("touchmove", function(event)
-					{
-					   event.preventDefault();
-					}, false);
-
-					// Steer the vehicle based on the phone orientation
-				});
-			});
-		
-		}
-		else
-		{
-			console.log("game");
-			socket.emit("device", {"type":"game"});
-			$("#mobileBegin").hide();
-
-			socket.on("initialize",function(data)
+			window.ondevicemotion = function(event) 
 			{
-				console.log("initialize");
-			});
-	
-			socket.on('turn', function(turn)
-			{
-				console.log("turn");
-				if(turn < -15)
+				ax = event.accelerationIncludingGravity.x
+				ay = event.accelerationIncludingGravity.y
+				az = event.accelerationIncludingGravity.z
+				rotation = event.rotationRate;
+				if (rotation != null) 
 				{
-					moveCharLeft();
+					arAlpha = Math.round(rotation.alpha);
+					arBeta = Math.round(rotation.beta);
+					arGamma = Math.round(rotation.gamma);
 				}
-				else if (turn > 15)
+				if(Math.abs(arBeta)>160)
 				{
-					moveCharRight();
+					//moveCharJump(event);
+				}
+			}
+			window.ondeviceorientation = function(event) 
+			{
+				alpha = Math.round(event.alpha);
+				beta = Math.round(event.beta);
+				gamma = Math.round(event.gamma);
+		
+				if(beta > 15)
+				{
+					//moveCharRight(event);
+					socket.emit("right",beta);
+				}
+				else if(beta<-15)
+				{
+					socket.emit("left",beta);
+					//moveCharLeft(event);
 				}
 				else
-				{				
-					stopCharMob();
+				{
+					socket.emit("stop",beta);
+					//stopCharMob(event);
 				}
-			});
-		}
+			}
+						
+			// If user touches the screen, accelerate
+			document.addEventListener("touchstart", function(event)
+			{
+			   socket.emit("accelerate", {'accelerate':true});
+			   $('#forward').addClass('active');
 
-		function moveCharJump(event) 
+			}, false);
+
+			// Stop accelerating if user stops touching screen
+			document.addEventListener("touchend", function(event)
+			{
+			   socket.emit("accelerate", {'accelerate':false});
+			   $('#forward').removeClass('active');
+			}, false);
+
+			// Prevent touchmove event from cancelling the 'touchend' event above
+			document.addEventListener("touchmove", function(event)
+			{
+			   event.preventDefault();
+			}, false);
+
+		});
+	}
+	else
+	{
+		console.log("game");
+		socket.emit("device", {"type":"game"});
+		$("#mobileBegin").hide();
+
+		socket.on("initialize",function(data)
 		{
-			jump = true;
-		}	
-		function moveCharRight() 
+			console.log("initialize");
+		});
+
+		socket.on('turn', function(turn)
 		{
-			RightIsPressed = true;
-			perso.style.transform = "perspective(700px) rotateY(0deg)";
-			perso.style.webkitTransform = "perspective(700px) rotateY(0deg)";
-		}	
-		function moveCharLeft() 
-		{
+			console.log("turn");
+			if(turn < -15)
+			{
+				moveCharLeft();
+			}
+			else if (turn > 15)
+			{
+				moveCharRight();
+			}
+			else
+			{				
+				stopCharMob();
+			}
+		});
+	}
+
+	function moveCharJump(event) 
+	{
+		jump = true;
+	}	
+	function moveCharRight() 
+	{
+		RightIsPressed = true;
+		perso.style.transform = "perspective(700px) rotateY(0deg)";
+		perso.style.webkitTransform = "perspective(700px) rotateY(0deg)";
+	}	
+	function moveCharLeft() 
+	{
+		LeftIsPressed = true;
+		perso.style.transform = "perspective(700px) rotateY(180deg)";
+		perso.style.webkitTransform = "perspective(700px) rotateY(180deg)";
+	}
+	function moveChar(event) 
+	{
+		if (event.keyCode == 37) 
+		{ 
 			LeftIsPressed = true;
 			perso.style.transform = "perspective(700px) rotateY(180deg)";
 			perso.style.webkitTransform = "perspective(700px) rotateY(180deg)";
 		}
-		function moveChar(event) 
-		{
-			if (event.keyCode == 37) 
-			{ 
-				LeftIsPressed = true;
-				perso.style.transform = "perspective(700px) rotateY(180deg)";
-				perso.style.webkitTransform = "perspective(700px) rotateY(180deg)";
-			}
-			if (event.keyCode == 38) 
-			{ 
-				jump = true;
-			}
-			if (event.keyCode == 39) 
-			{ 
-				RightIsPressed = true;
-				perso.style.transform = "perspective(700px) rotateY(0deg)";
-				perso.style.webkitTransform = "perspective(700px) rotateY(0deg)";
-			}
+		if (event.keyCode == 38) 
+		{ 
+			jump = true;
 		}
-		function stopChar(event) 
+		if (event.keyCode == 39) 
+		{ 
+			RightIsPressed = true;
+			perso.style.transform = "perspective(700px) rotateY(0deg)";
+			perso.style.webkitTransform = "perspective(700px) rotateY(0deg)";
+		}
+	}
+	function stopChar(event) 
+	{
+		LeftIsPressed = false;
+		RightIsPressed = false;	
+	}
+
+	function stopCharMob() 
+	{
+		LeftIsPressed = false;
+		RightIsPressed = false;	
+	}
+
+	function createCloud(time) 
+	{
+		var n = document.createElement("img");
+		var nScale = 0.5 + Math.random() * .5;
+		n.src = "img/nuage.png";
+		n.alt = "nuage";
+		n.style.width = 150 * nScale + "px";
+		if (time == "init") n.style.left = Math.random() * (window.innerWidth - n.offsetWidth) + "px";
+		else n.style.left = -150 * nScale + "px";
+		n.style.top = Math.random() * (clouds.offsetHeight - 60 * nScale) + "px";
+		n.style.opacity = 1 * nScale;
+		clouds.appendChild(n);
+	}
+
+	function updateClouds() 
+	{
+		for (i = 0; i < clouds.children.length; i++) 
 		{
-			LeftIsPressed = false;
-			RightIsPressed = false;	
+			clouds.children[i].style.left = clouds.children[i].offsetLeft + vCloud * clouds.children[i].style.opacity + "px";
+			if (clouds.children[i].offsetLeft > window.innerWidth) clouds.removeChild(clouds.children[i]);
 		}
 
-		function stopCharMob() 
+		if (clouds.children.length < nbClouds) 
 		{
-			LeftIsPressed = false;
-			RightIsPressed = false;	
-		}
-
-		function createCloud(time) 
-		{
-			var n = document.createElement("img");
-			var nScale = 0.5 + Math.random() * .5;
-			n.src = "img/nuage.png";
-			n.alt = "nuage";
-			n.style.width = 150 * nScale + "px";
-			if (time == "init") n.style.left = Math.random() * (window.innerWidth - n.offsetWidth) + "px";
-			else n.style.left = -150 * nScale + "px";
-			n.style.top = Math.random() * (clouds.offsetHeight - 60 * nScale) + "px";
-			n.style.opacity = 1 * nScale;
-			clouds.appendChild(n);
-		}
-
-		function updateClouds() 
-		{
-			for (i = 0; i < clouds.children.length; i++) 
+			var nb = nbClouds - clouds.children.length;
+			for (i = 0; i < nb; i++) 
 			{
-				clouds.children[i].style.left = clouds.children[i].offsetLeft + vCloud * clouds.children[i].style.opacity + "px";
-				if (clouds.children[i].offsetLeft > window.innerWidth) clouds.removeChild(clouds.children[i]);
-			}
-	
-			if (clouds.children.length < nbClouds) 
-			{
-				var nb = nbClouds - clouds.children.length;
-				for (i = 0; i < nb; i++) 
-				{
-					createCloud("game");
-				}
+				createCloud("game");
 			}
 		}
+	}
 
-		function updatePerso() 
+	function updatePerso() 
+	{
+		position[0] = perso.offsetLeft;
+		position[1] = perso.offsetTop;
+		if (LeftIsPressed) 
 		{
-			position[0] = perso.offsetLeft;
-			position[1] = perso.offsetTop;
-			if (LeftIsPressed) 
+			if (position[0] + acceleration[0] - vPerso > 0) velocity[0] -= vPerso;
+			else 
 			{
-				if (position[0] + acceleration[0] - vPerso > 0) velocity[0] -= vPerso;
-				else 
-				{
-					acceleration[0] = 0;
-					velocity[0] = 0;
-					position[0] = 0;
-				}
+				acceleration[0] = 0;
+				velocity[0] = 0;
+				position[0] = 0;
 			}
-			if (RightIsPressed) 
-			{
-				if (position[0] + acceleration[0] + vPerso + perso.offsetWidth < window.innerWidth) velocity[0] += vPerso;
-				else 
-				{
-					acceleration[0] = 0;
-					velocity[0] = 0;
-					position[0] = window.innerWidth - perso.offsetWidth;
-				}
-			}			
-			if (jump) 
-			{			
-				jumpCount++;
-				if (jumpCount <= nbJumps) acceleration[1] = -jumpHeight;
-				isJumping = true;
-				jump = false;
-			}
-			if (isJumping) 
-			{
-				if (position[1] + acceleration[1] + gravity < ground) acceleration[1] += gravity;
-				else 
-				{
-					acceleration[1] = 0;
-					position[1] = ground;
-					isJumping = false;
-					jumpCount = 0;
-				}
-			}
-			velocity[0] += acceleration[0];
-			velocity[1] += acceleration[1];
-			position[0] += velocity[0];
-			position[1] += velocity[1];
-			velocity = [0,0];
-	
-			perso.style.left = position[0] + "px";
-			perso.style.top = position[1] + "px";
 		}
-
-		function drawPerso() 
+		if (RightIsPressed) 
 		{
-			if (isJumping) 
+			if (position[0] + acceleration[0] + vPerso + perso.offsetWidth < window.innerWidth) velocity[0] += vPerso;
+			else 
 			{
-				dPerso = 4;
+				acceleration[0] = 0;
+				velocity[0] = 0;
+				position[0] = window.innerWidth - perso.offsetWidth;
+			}
+		}			
+		if (jump) 
+		{			
+			jumpCount++;
+			if (jumpCount <= nbJumps) acceleration[1] = -jumpHeight;
+			isJumping = true;
+			jump = false;
+		}
+		if (isJumping) 
+		{
+			if (position[1] + acceleration[1] + gravity < ground) acceleration[1] += gravity;
+			else 
+			{
+				acceleration[1] = 0;
+				position[1] = ground;
+				isJumping = false;
+				jumpCount = 0;
+			}
+		}
+		velocity[0] += acceleration[0];
+		velocity[1] += acceleration[1];
+		position[0] += velocity[0];
+		position[1] += velocity[1];
+		velocity = [0,0];
+
+		perso.style.left = position[0] + "px";
+		perso.style.top = position[1] + "px";
+	}
+
+	function drawPerso() 
+	{
+		if (isJumping) 
+		{
+			dPerso = 4;
+			stepAnim = frames;
+		}
+		else if (LeftIsPressed || RightIsPressed) 
+		{
+			if (frames % sequence == 0) 
+			{
+				dPerso = (dPerso == 2) ? 3 : 2;
 				stepAnim = frames;
 			}
-			else if (LeftIsPressed || RightIsPressed) 
+		}
+		else 
+		{
+			if (dPerso == 1) 
 			{
-				if (frames % sequence == 0) 
+				if (frames - stepAnim == sequence) 
 				{
-					dPerso = (dPerso == 2) ? 3 : 2;
+					dPerso = 0;
 					stepAnim = frames;
 				}
 			}
 			else 
 			{
-				if (dPerso == 1) 
+				if (frames - stepAnim == breathRythm) 
 				{
-					if (frames - stepAnim == sequence) 
-					{
-						dPerso = 0;
-						stepAnim = frames;
-					}
+					dPerso = 1;
+					stepAnim = frames
 				}
-				else 
-				{
-					if (frames - stepAnim == breathRythm) 
-					{
-						dPerso = 1;
-						stepAnim = frames
-					}
-					else dPerso = 0;
-				}
-			}				
-			//perso.style.background = 'url("/img/perso.png") ' + (-120 * dPerso) + 'px 0px no-repeat';
-		}
+				else dPerso = 0;
+			}
+		}				
+		//perso.style.background = 'url("/img/perso.png") ' + (-120 * dPerso) + 'px 0px no-repeat';
+	}
 
-		function loop() 
-		{
-			updatePerso();
-			drawPerso();
-	
-			updateClouds();
-	
-			frames++;
-		}
-	};
+	function loop() 
+	{
+		updatePerso();
+		drawPerso();
+
+		updateClouds();
+
+		frames++;
+	}
+};
 
 
