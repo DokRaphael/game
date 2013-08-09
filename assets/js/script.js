b42bad49639105cacef6eef5ac345ccdbcf26048
dook0 <dook0@hotmail.com>
2013-08-09 14:30:08 +0200

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -276,11 +276,12 @@ $(function()
 		}
 		if (RightIsPressed) 
 		{
-			console.log("rigth!!!!");
-			console.log(position[0] + acceleration[0] + vPerso + perso.offsetWidth + " // "+ window.innerWidth);
-			console.log(velocity[0] + " // "+vPerso);
+		//	console.log(position[0] + acceleration[0] + vPerso + perso.offsetWidth + " // "+ window.innerWidth);
+		//	console.log(velocity[0] + " // "+vPerso);
 			if (position[0] + acceleration[0] + vPerso + perso.offsetWidth < window.innerWidth)
 			{ 
+						console.log("rigth!!!!");
+
 				velocity[0] = vPerso + velocity[0];
 			}
 			else 
